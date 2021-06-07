efe4f5ad420bbd19362318f35e1e11eba538e6e6
fatma-armi <55367488+fatma-armi@users.noreply.github.com>
2021-06-07 12:26:15 +0700

    Delete Pertemuan13 directory

--- Pertemuan13/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once("koneksi.php");
-$id_tma = $_GET['id_tma'];
-$result = mysqli_query($mysqli, "DELETE FROM tma WHERE id_tma='$id_tma'");
-if($result){
-	header("Location:index.php");
-}
-?>
